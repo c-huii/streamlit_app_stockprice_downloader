b4cccfda8384cd2821633284b5cf4901b4d74fa0
c-huii <127276334+c-huii@users.noreply.github.com>
2024-11-23 04:42:36 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 import matplotlib.ticker as ticker
 
 # 폰트 설정 (한글 깨짐 방지)
